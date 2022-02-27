b7f2e4ed70f580c7c717117a3a35c9ab3d4ddd16
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2022-02-27 01:06:42 -0300

    Funcionamento dos Botões

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -58,29 +58,39 @@ export const TransactionTypeContainer = styled.div`
   gap: 0.5rem;
 
   button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  }
+`;
 
-    transition: border-color 0.2s;
+interface RadioBoxProps {
+  isActive: boolean;
+}
 
-    &:hover {
-      border-color: ${darken(0.1, '#d7d7d7')};
-    }
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    img {
-      height: 20px;
-      width: 20px;
-    }
+  background: ${(props) => (props.isActive ? '#ccc' : 'transparent')};
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')};
+  }
+
+  img {
+    height: 20px;
+    width: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
