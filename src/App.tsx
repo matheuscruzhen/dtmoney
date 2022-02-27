dc8328f0287a63100cd20f1abea2de0de965234a
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2022-02-26 23:51:10 -0300

    Componente NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
 
@@ -21,16 +22,16 @@ export function App() {
 
   return (
     <>
-      <GlobalStyle />
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalIsOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      />
+
+      <GlobalStyle />
     </>
   );
 }
