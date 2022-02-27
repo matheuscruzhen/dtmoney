91e6abd1a2c1f7ac810e8bc8e825fbd9ae0ac33b
Matheus Henrique da Cruz Souza <matheuscruzhen@gmail.com>
2022-02-27 15:56:27 -0300

    A Context API no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
+
+import { TransactionsContext } from './TransactionsContext';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
@@ -21,7 +24,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -32,6 +35,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
